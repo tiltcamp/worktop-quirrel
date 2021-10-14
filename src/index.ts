9c4e402031024aed65291e36120cecb40edce213
Matt Webb <hello@mattwebb.io>
2021-10-13 21:16:42 -0700

    Update `process.env` hack to tested working method

--- src/index.ts
+++ src/index.ts
@@ -15,7 +15,7 @@ import {
  */
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore
-self.process.env = self;
+self.process = { env: self };
 
 /*
   Without defining `window`, Workers will raise `window is not defined`.
